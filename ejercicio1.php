5825fb1f13b92f7d43fe66443e44743dd7808262
cperezll <cperez2826gmail.com>
2018-08-09 13:17:05 +0200

    Finalización del ejercico 1

---
+++ ejercicio1.php
@@ -0,0 +1,24 @@
+
+<h2> Ejercicio 1: Aprendizaje PHP</h2>
+
+<?php 
+ /*Comentario multilenea.
+ 	Vamos a crear un primer mensaje con unas variables. En concreto dos variables
+ */
+
+	echo "Mi primer programa en php"."<br/>";
+
+	//Variable de tipo String
+	$uno = "Contendio de la variable 1 es el siguiente";
+	echo "============================================="."<br/>";
+
+	//Integer
+	$dos = 245;
+
+	//Mostramos el valor de las dos variables definidas
+	echo "Uno: ".$uno."<br/>";
+	echo "Dos: ".$dos."<br/>"; //hr. es una línea
+	echo "=============================================";
+	echo "<hr/>";
+	
+ ?>
