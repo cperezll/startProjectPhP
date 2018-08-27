03c5a52743bd037992b241b917e889a5c6930aee
cperezll <cperez2826gmail.com>
2018-08-27 09:05:14 +0200

    new exercice 4

--- ejercicio2.php
+++ ejercicio2.php
@@ -1,13 +1,27 @@
-<?php 
 
-	//Hacemos un script nuevo
-	/* En este script loq ue vamos ahcer es implementar en un for 
-	el augmento de un numero hasta un maximo, para indicar su potencial */
+<?php 
+/*
+************************************************************************************************************
+************************************************************************************************************
+************								CODIGO DEL EJERCICIO 2								************
+************************************************************************************************************	
+************************************************************************************************************
+_____________________________
+Autor:Cristian Perez Llurba
+Fecha: 09-08-2018
+Localización: BCN
+_____________________________
+
+D E S C R I P C I O N
+	Hacemos un script nuevo.
+ 	En este script lo que vamos ahcer es implementar en un for 
+	el augmento de un numero hasta un maximo, para indicar su potencia.
+*/
 	
 	for ($i = 0; $i <= 29; $i++) {
 		echo "===================================================================="."</br>";
 		echo "La variable <b>i</b>, tiene el siguiente valor: ".$i."</br>";
-		echo "El valor del cuadrado de <b>i</b>, tiene el valor: ".($i*$i)."</br>";
+		echo "El valor del cuadrado de <b>i</b>, tiene el valor: ".($i=$i)."</br>";
 		if ($i==29){
 			echo "====================================================================="."</br>";
 		}
--- ejercicio3.php
+++ ejercicio3.php
@@ -1,3 +1,5 @@
+
+<?php 
 /*
 ************************************************************************************************************
 ************************************************************************************************************
@@ -9,10 +11,10 @@ Autor:Cristian Perez Llurba
 Fecha: 09-08-2018
 Localización: BCN
 _____________________________
-*/
 
-<?php 
-	//Modificacion del ejercicio numero2. para obtener un condicional, obteniendo un numero para o impar
+D E S C R I P C I O N
+	Modificacion del ejercicio número2. para obtener un condicional, obteniendo un numero par o impar
+*/
 
 	for($a = 0; $a <= 9 ;$a++){
 	
@@ -20,11 +22,11 @@ _____________________________
 
 		echo "El cuadrado de <b>".$a."</b>, es el siguiente: <i>".$cuadrado."</i>";
 		
-		//Deteción del numero par
+		//Deteción del número par
 		if($cuadrado%2==0){
-			echo ". Y el numero es par."."</br>";
+			echo ". Y el número es par."."</br>";
 		}else{
-			echo ". Y el numero no es par."."</br>";
+			echo ". Y el número no es par."."</br>";
 		}
 	}
 
